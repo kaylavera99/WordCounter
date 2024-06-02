fa53b08a9d9fb7486f22a4b0e6dbe93c38fe45e5
kaylavera99 <kaylavd99@gmail.com>
2024-06-02 16:06:38 -0400

    Added counting letters input by user
    Refers to issue #2

--- wordcounter.py
+++ wordcounter.py
@@ -1,15 +1,16 @@
 def main():
-    print("This program counts the number of vowels in an input phrase")
-    inputPhrase = input("Enter your phrase: ").lower()
+    print("This program counts the number of given letters in an input phrase")
+    lettersCount = input("Enter the letters to count (e.g., 'aeiou'): ").lower()
+    inputPhrase = input("Enter your phrase: ")
 
 
-    totalVowels = 0
+    totalOccurencesOfLettersToCount = 0
     for character in inputPhrase:
-        if character in ['a', 'e', 'i', 'o', 'u']:
-            totalVowels = totalVowels + 1
+        if character in lettersCount:
+            totalOccurencesOfLettersToCount = totalOccurencesOfLettersToCount + 1
 
     
-    print("Total vowels in your phrase: {}".format(totalVowels))
+    print("Total occurences of '{}' in your phrase: {}".format(lettersCount, totalOccurencesOfLettersToCount))
 
 
 if __name__ == "__main__":
