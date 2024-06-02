ee9391ae6839eafc0c3fd346ced229c05ea462b7
kaylavera99 <kaylavd99@gmail.com>
2024-06-02 15:51:04 -0400

    Refactoring vowel counting
    This references issue #1

--- wordcounter.py
+++ wordcounter.py
@@ -2,16 +2,21 @@ def getInputPhrase():
      return input("Enter your phrase: ").lower()
 
 
+def getVowelCount(phrase):
+    vowelCount = 0
+    for character in phrase:
+        if character in ['a', 'e', 'i', 'o', 'u']:
+            vowelCount = vowelCount + 1
+    return vowelCount
+
 def main():
     print("This program counts the number of vowels in an input phrase")
     inputPhrase = getInputPhrase()
     
+    totalVowels = getVowelCount(inputPhrase)
 
 
-    totalVowels = 0
-    for character in inputPhrase:
-        if character in ['a', 'e', 'i', 'o', 'u']:
-            totalVowels = totalVowels + 1
+   
 
     
     print("Total vowels in your phrase: {}".format(totalVowels))
